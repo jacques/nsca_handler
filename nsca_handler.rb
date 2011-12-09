4d20dc0760c9a914a1119a6f05789d30e4cc759f
Jacques Marneweck <jacques@powertrip.co.za>
2011-12-09 18:47:16 +0200

    Fixed some typos and reduce some white space in config section

--- nsca_handler.rb
+++ nsca_handler.rb
@@ -11,17 +11,17 @@ class NSCAHandler < Chef::Handler
     @c_elapsed_time  = Chef::Config['elapsed_time_critical']      ||  60 
     @c_updated_res   = Chef::Config['updated_resources_critical'] ||  20 
     @w_updated_res   = Chef::Config['updated_resources_warning']  ||  5
-    @send_nsca_binary= Chef::Config['send_nsca_binary']                || '/usr/sbin/send_nsca'
-    @service_name    = Chef::Config['service_name']                    || 'Chef client run status'
+    @send_nsca_binary= Chef::Config['send_nsca_binary']           || '/root/bin/send_nsca'
+    @service_name    = Chef::Config['service_name']               || 'Chef client run status'
 
   end
   def report
     ret=''
     if run_status.failed? || (run_status.elapsed_time > @c_elapsed_time.to_i ) || (run_status.updated_resources.length > @c_updated_res.to_i)
       ret=2 
-      Chef::Log.info( "Setiing host status critincal:#{run_status.failed?} | #{run_status.elapsed_time} | #{@c_elapsed_time.to_i} | #{run_status.updated_resources.length} | #{@c_updated_res.to_i}")
+      Chef::Log.info( "Setting host status critical: #{run_status.failed?} | #{run_status.elapsed_time} | #{@c_elapsed_time.to_i} | #{run_status.updated_resources.length} | #{@c_updated_res.to_i}")
     elsif (run_status.elapsed_time > @w_elapsed_time.to_i) || (run_status.updated_resources.length > @w_updated_res.to_i)
-      Chef::Log.info( "Setting host status warning  : #{run_status.elapsed_time} | #{@w_elapsed_time.to_i} | #{run_status.updated_resources.length}")
+      Chef::Log.info( "Setting host status warning: #{run_status.elapsed_time} | #{@w_elapsed_time.to_i} | #{run_status.updated_resources.length}")
       ret=1
     else
       ret=0
